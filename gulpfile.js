c16b905ca4f04f105edc6b36943c4cf92193703a
rharriso <rtharrison86@gmail.com>
2016-09-24 18:43:30 +0000

    convert to browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,39 @@
 var gulp        = require("gulp");
 var gls         = require('gulp-live-server');
 var sourcemaps  = require('gulp-sourcemaps');
-var browserify  = require('gulp-browserify');
-var react       = require('gulp-react');
-var babel       = require('gulp-babel');
+var browserify  = require('browserify');
+var babel       = require('babelify');
 var gutil       = require('gulp-util');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var reactify    = require('reactify');
+var rename      = require('gulp-rename');
 
 var livereload = require('gulp-livereload');
 
 var paths = {
-    client: ["./client-src/**/*.js"],
+    clientSrc: ["./client-src/**/*.js"],
     server: ["./server/**/*", "index.js"] 
 };
 
 gulp.task('scripts', function(){
-   gulp.src('./client-src/app.js') 
-        .on('error', gutil.log) 
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-          presets: ["es2015", "react"]
-        }).on('error', gutil.log))
-        .pipe(browserify({
-          debug : !gulp.env.production
-        }).on('error', gutil.log))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./client/js'))
-        .pipe(livereload({start: true, port: 8081}));
+  
+  var b = browserify({
+    debug: true,
+    sourceMaps: true,
+    entries: "./client-src/app.js" 
+  }).transform(babel, {presets: ["es2015", "react"]})
+    .transform(reactify);
+  
+  b.bundle() 
+    .on('error', gutil.log)
+    .pipe(source("./client-src/app.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(rename({dirname: ''}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./client/js/"))
+    .pipe(livereload({start: true, port: 8081}));
 });
 
 /*
@@ -37,7 +45,7 @@ gulp.task('serve', ['scripts'], function() {
   
   livereload.listen({port: 8081});
    
-  gulp.watch(paths.client, ['scripts']);
+  gulp.watch(paths.clientSrc, ['scripts']);
   gulp.watch(paths.server, function(file){
     server.start();  
     server.notify(file);  
