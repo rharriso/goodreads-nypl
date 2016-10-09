c271c786ed89b97f26b1ec8fdad2537a97406ef8
Ross Harrison <rtharrison86@gmail.com>
2016-10-09 00:15:55 -0400

    add audiobook link, can't tell yet (NYPL site slow)

--- client-src/BookListItem.js
+++ client-src/BookListItem.js
@@ -1,10 +1,19 @@
-import ListItem from 'material-ui/List/ListItem';
 import React from 'react';
+
 import AppDispatcher from './Dispatchers/AppDispatcher';
+import FlatButton from 'material-ui/FlatButton';
+import ListItem from 'material-ui/List/ListItem';
 
 var BookListItem = React.createClass({
+
 	render: function(){
-		return <ListItem>{this.props.book.title}</ListItem>
+		return <ListItem>{this.props.book.title}
+      
+      <FlatButton
+        label="audiobook" 
+        href={'http://browse.nypl.org/iii/encore/search/C__S['+this.props.book.title+']__Orightresult__U?lang=eng&suite=def'}
+      />
+    </ListItem>
 	},
 });
 
