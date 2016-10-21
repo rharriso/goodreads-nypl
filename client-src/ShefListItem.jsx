e7ba2da339799e7717ef4c3670beb612178eea47
rharriso <rtharrison86@gmail.com>
2016-10-21 04:22:06 +0000

    use es6 classes

--- client-src/ShefListItem.jsx
+++ client-src/ShefListItem.jsx
@@ -2,17 +2,17 @@ import ListItem from 'material-ui/List/ListItem';
 import React from 'react';
 import AppDispatcher from './Dispatchers/AppDispatcher';
 
-var ShelfListItem = React.createClass({
-	render: function(){
-		return <ListItem onClick={this.onClick}>{this.props.shelf.title}</ListItem>
-	},
+class ShelfListItem extends React.Component {
+	render(){
+		return <ListItem onClick={this.onClick.bind(this)}>{this.props.shelf.title}</ListItem>
+	}
 	
-	onClick: function(event){
+	onClick(event){
 		AppDispatcher.handleViewAction({
 			actionType: "CURR_SHELF_CHANGE",
 			shelfName: this.props.shelf.title
 		});
 	}
-});
+}
 
 export default ShelfListItem;
