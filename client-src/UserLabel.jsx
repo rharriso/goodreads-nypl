9aa534ae7924b04bba782b526640e96af6cbb473
rharriso <rtharrison86@gmail.com>
2016-10-31 02:14:12 +0000

    working in the user store

--- client-src/UserLabel.jsx
+++ client-src/UserLabel.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import AppDispatcher from './Dispatchers/AppDispatcher';
 
 /**
  * UserLabel
  */
 class UserLabel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }		
+	
+	onSubmit(){
+		alert("HEYO: " + this.state.username);
+	}
+	
+	handleChange(e){
+		console.log(this.state.username);
+		this.setState({username: e.target.value})
+	}
+	
   render(){
-    
+		if (this.props.user) {
+				
+		} else {
+			return (
+				<form onSubmit={this.onSubmit.bind(this)}>
+					<input placeholder='username' value={this.state.username} onChange={this.handleChange.bind(this)} />
+					<input type="submit" value="find user" />
+				</form>
+			);
+		}
   }
 }
 
