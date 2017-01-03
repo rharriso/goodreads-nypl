4426d59dafa89986379bdcb2801e780da461d373
Ross Harrison <rtharrison86@gmail.com>
2017-01-03 18:31:32 -0500

    shelf list not loading right now

--- client-src/ShelfListItem.jsx
+++ client-src/ShelfListItem.jsx
@@ -7,14 +7,20 @@ import { actions } from './Stores/application-store';
 
 class ShelfListItem extends React.Component {
   onClick(){
-  	fetch(`/shelf/${this.props.shelf.title}`)
-  		.then((response) => response.json())
+    fetch(`/shelf/${this.props.shelf.title}?userId=${this.props.userId}`)
+      .then((response) => response.json())
       .then((shelfData) => this.props.dispatch(actions.setShelf(shelfData)));
   }
 
   render(){
-    return <ListItem onClick={this.onClick.bind(this)}>{this.props.shelf.title}</ListItem>
+    return <ListItem onClick={this.onClick.bind(this)}>{this.props.shelf.title}</ListItem>;
   }
 }
 
-export default connect()(ShelfListItem);
+const mapStateToProps = function (state) {
+  return {
+    userId: state.user && state.user.id
+  };
+};
+
+export default connect(mapStateToProps)(ShelfListItem);
